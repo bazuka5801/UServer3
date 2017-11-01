179b768cf17b88261332b350995dcc433b3103a6
Maxim <bazuka5801@mail.ru>
2017-11-01 21:09:55 +0300

    Update database

--- UServer3/UServer3/Data/Database.cs
+++ UServer3/UServer3/Data/Database.cs
@@ -6,13 +6,13 @@ namespace UServer3.Data
     public class Database
     {
         public static bool IsCollectable(UInt32 uid) => DB_Collectables.Contains(uid);
-        public static bool IsBaseResource(UInt32 uid) => DB_BaseResource.Contains(uid);
-        public static bool IsBaseOre(UInt32 uid) => DB_BaseOre.Contains(uid);
-        public static bool IsComponent(Int32 id) => DB_Component.Contains(id);
+        public static bool IsBaseResource(UInt32 uid) => DB_BaseResources.Contains(uid);
+        public static bool IsBaseOre(UInt32 uid) => DB_BaseOres.Contains(uid);
+        public static bool IsComponent(Int32 id) => DB_Components.Contains(id);
         
         private static HashSet<UInt32> DB_Collectables;
-        private static HashSet<UInt32> DB_BaseResource;
-        private static HashSet<UInt32> DB_BaseOre;
-        private static HashSet<Int32> DB_Component;
+        private static HashSet<UInt32> DB_BaseResources;
+        private static HashSet<UInt32> DB_BaseOres;
+        private static HashSet<Int32> DB_Components;
     }
 }
