3b331d7b026b40820ec586344335a67bd5551fb8
Maxim <bazuka5801@mail.ru>
2017-11-04 02:34:07 +0300

    Remove Halloween collectibles

--- UServer3/init.bat
+++ UServer3/init.bat
@@ -1,4 +1,12 @@
 : Create/Update build folder
+
+RMDIR /S /Q Builded
+IF EXIST Builded (
+	echo msgbox "Can't remove 'Builded' folder!" > %tmp%\tmp.vbs
+	cscript /nologo %tmp%\tmp.vbs
+	del %tmp%\tmp.vbs
+)
+
 if not exist "Builded\" mkdir Builded
 if not exist "Builded\Data\" mkdir Builded\Data
 if not exist "Builded\Data\Bin" mkdir Builded\Data\Bin
